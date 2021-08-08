0976558fe7a2d81faefefe3d23ef8790e8733d9b
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-08-08 07:17:08 +0000

    initial setup for ui examples

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>NEXT UI EXAMPLES</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to NEXT UI Examples
+        </h1>
+
+      </main>  
+
+        
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", "system-ui", "sans-serif"],
+        mono: ["Menlo", ...defaultTheme.fontFamily.mono],
+        source: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        "ubuntu-mono": ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
+        system: defaultTheme.fontFamily.sans,
+        flow: "Flow",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        gray: colors.coolGray,
+        red: colors.red,
+        yellow: colors.amber,
+        blue: colors.blue,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
